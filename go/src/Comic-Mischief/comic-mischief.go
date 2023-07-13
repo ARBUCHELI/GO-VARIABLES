d15a6ad7054f7282e5a3c89f65f11aa8e7793392
ARBUCHELI <radio_seattle@hotmail.com>
2023-07-13 19:11:06 +0300

    feat: variables and types - project comic mieschief

---
+++ go/src/Comic-Mischief/comic-mischief.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+  
+func main () {
+  var publisher, writer, artist, title string
+  var year, pageNumber uint
+  var grade float32
+
+  title = "Mr. GoToSleep"
+  writer = "Tracey Hatchet"
+  artist = "Jewel Tampson"
+  publisher = "DizzyBooks Publishing Inc."
+  year = 1997
+  pageNumber = 14
+  grade = 6.5
+
+  fmt.Println(title, "Written By", writer, "Drawn By", artist, "Published in", year, "Number of Pages", pageNumber, "Published by", publisher, "Rate", grade)
+
+  fmt.Println("")
+
+  title = "Epic Vol. 1"
+  writer = "Ryan N. Shawn"
+  artist = "Phoebe Paperclips"
+  publisher = "DizzyBooks Publishing Inc."
+  year = 2013
+  pageNumber = 160
+  grade = 9.0
+
+   fmt.Println(title, "Written By", writer, "Drawn By", artist, "Published in", year, "Number of Pages", pageNumber, "Published by", publisher, "Rate", grade)
+}
