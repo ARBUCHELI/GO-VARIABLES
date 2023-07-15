c6141fb78b449803625b2a558cc51e7864eb7609
ARBUCHELI <radio_seattle@hotmail.com>
2023-07-15 21:48:39 +0300

    feat: fmt package - the printf method

---
+++ go/src/fmt3/fmt3.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+  animal1 := "cat"
+  animal2 := "dog"
+  
+  // Add your code below:
+  fmt.Printf("are you a %v or a %v person?", animal1, animal2)
+}
